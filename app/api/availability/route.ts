48a2a97fd9d66a395720a03c9bd9460751b11810
TAWSIF AHMAD IMON <taim@TAWSIFs-MacBook-Pro.local>
2026-03-10 22:04:20 +0100

    fix availability API

--- app/api/availability/route.ts
+++ app/api/availability/route.ts
@@ -1,22 +1,29 @@
 import { prisma } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
-export async function POST(req:Request){
+export async function POST(req: Request) {
 
-const {roomId,checkin,checkout} = await req.json()
+  const { roomId, checkin, checkout } = await req.json()
 
-const existing = await prisma.booking.findFirst({
-where:{
-roomId,
-OR:[
-{
-checkin:{lte:new Date(checkout)},
-checkout:{gte:new Date(checkin)}
-}
-]
-}
-})
+  const existing = await prisma.booking.findFirst({
+    where: {
+      roomId: roomId,
+      OR: [
+        {
+          checkIn: {
+            lte: new Date(checkout)
+          }
+        },
+        {
+          checkOut: {
+            gte: new Date(checkin)
+          }
+        }
+      ]
+    }
+  })
 
-return NextResponse.json({available:!existing})
-
+  return NextResponse.json({
+    available: !existing
+  })
 }
